0bf468b49b5bc8a5432508eed5a14e4d8311118f
Dazhong Xia <dazhong.xia@catalyst.coop>
2025-09-02 12:06:47 -0400

    feat: add user ID to logs if available.

--- eel_hole/__init__.py
+++ eel_hole/__init__.py
@@ -38,9 +38,22 @@ AUTH0_DOMAIN = os.getenv("PUDL_VIEWER_AUTH0_DOMAIN")
 CLIENT_ID = os.getenv("PUDL_VIEWER_AUTH0_CLIENT_ID")
 CLIENT_SECRET = os.getenv("PUDL_VIEWER_AUTH0_CLIENT_SECRET")
 
+
+def user_id_adder(logger, log_method, event_dict):
+    """Add user ID to log if available.
+
+    Must be added to processor list *before* JSONRenderer, otherwise event_dict
+    will be rendered to string already.
+    """
+    if current_user:
+        event_dict["user_id"] = current_user.get_id()
+    return event_dict
+
+
 structlog.configure(
     processors=[
         structlog.processors.TimeStamper(fmt="iso"),
+        user_id_adder,
         structlog.processors.JSONRenderer(),
     ],
     wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),

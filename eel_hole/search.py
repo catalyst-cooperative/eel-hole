bcf144db5a459841d26fd43286698aa75c7917d3
Dazhong Xia <dazhong.xia@catalyst.coop>
2026-02-11 13:11:10 -0500

    feat: increase search result page limit to 50
    * feat: stop paginating and just give all the results every time
    
    * feat: make page limit 50 instead of infinity

--- eel_hole/search.py
+++ eel_hole/search.py
@@ -101,9 +101,11 @@ def run_search(
         preliminary_penalty = Term("tags", "preliminary", boost=-10.0)
         query_with_boosts = AndMaybe(user_query, Or([out_boost, preliminary_penalty]))
         if not is_flag_enabled("ferc_enabled"):
-            results = searcher.search(query_with_boosts, filter=Term("package", "pudl"))
+            results = searcher.search(
+                query_with_boosts, filter=Term("package", "pudl"), limit=50
+            )
         else:
-            results = searcher.search(query_with_boosts)
+            results = searcher.search(query_with_boosts, limit=50)
         for hit in results:
             log.debug(
                 "hit",

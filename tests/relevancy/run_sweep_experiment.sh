1d458876abf9f72ce54bb149af6e3d7f8a50508c
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2026-04-03 13:51:59 -0400

    oops missed a variable

--- tests/relevancy/run_sweep_experiment.sh
+++ tests/relevancy/run_sweep_experiment.sh
@@ -26,6 +26,6 @@ for k in 0 1 2 3 4 5 6 7 8 9; do
     next_stem="${PREFIX}$i"
     set -x
     echo "$next_stem" | uv run tests/relevancy/sweep.py $input_step
-    time uv run pytest tests/relevancy/test_relevancy.py::test_sweep --experiment tests/relevancy/${next_stem}.yaml
+    time uv run pytest tests/relevancy/test_relevancy.py::test_sweep --experiment ${STEP_PATH}/${next_stem}.yaml
     unset -x
 done

e8d1f1331e322d8a5b88bfd3f3490062c3abddf6
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2026-06-26 09:49:42 -0400

    Add integration test for round-trip filter serialization deserialization

--- tests/integration/test_preview.py
+++ tests/integration/test_preview.py
@@ -99,23 +99,41 @@ def test_filter_serialization_roundtrip(page: Page):
     _ = page.goto("http://localhost:8080/login")
     _ = page.goto("http://localhost:8080/preview/pudl/core_pudl__codes_datasources")
 
+    # this string should only appear when we have the filter active
+    canary = page.get_by_text("Showing 1 rows out of 1 rows that match your filters")
 
     ag_root = page.locator("#data-table .ag-root-wrapper")
     expect(ag_root).to_be_visible()
 
-    datasource_header = page.get_by_role('columnheader', name="datasource")
-    filter_button = page.locator("//*[@data-ref='eFilterButton']")
-    filter_input = page.locator("//*[@data-ref='eInput']")
+    # on initial load: not filtered; shows all data sources
+    expect(canary).not_to_be_visible()
 
-    datasource_filter = datasource_header.locator(filter_button)
-    expect(datasource_filter).to_be_visible()
-    datasource_filter.click()
+    datasource_header = page.get_by_role("columnheader", name="datasource")
+    show_filter_button = page.locator("//*[@data-ref='eFilterButton']")
+    filter_input = page.locator("//*[@data-ref='eInput']").filter(visible=True)
+    filter_apply = page.get_by_role("button", name="Apply")
 
-    datasource_filter_input = datasource_header.locator(filter_input)
-    expect(datasource_filter_input).to_be_visible(timeout=5_000)
-    # select just ferc
-    # class=ag-header-cell
-    # col-id=datasource
-    # grab the page location
-    # go to page location
-    # ensure just ferc is selected
+    # click header button to show filter panel
+    show_datasource_filter = datasource_header.locator(show_filter_button)
+    expect(show_datasource_filter).to_be_visible()
+    show_datasource_filter.click()
+
+    # configure filter to show only one data source
+    datasource_filter = page.get_by_label("Column Filter")
+    expect(datasource_filter).to_be_visible(timeout=5_000)
+    datasource_filter.locator(filter_input).fill("epacems")
+    datasource_filter.locator(filter_apply).click()
+
+    # ensure filter has taken effect
+    expect(canary).to_be_visible(timeout=5_000)
+
+    # record url containing encoded filter
+    serialized = page.url
+
+    # go away; filter clears
+    page.goto("http://localhost:8080/preview/pudl/core_pudl__codes_datasources")
+    expect(canary).not_to_be_visible()
+
+    # return to serialized URL; filter re-engaged
+    _ = page.goto(serialized)
+    expect(canary).to_be_visible()
